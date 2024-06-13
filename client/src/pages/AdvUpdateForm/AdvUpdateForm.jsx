41b7dfc8660ec69c2386c313d87f6638b4d8e179
lukas02727 <lukashodovnik@gmail.com>
2024-06-13 17:58:34 +0200

    uprava_inzeratu_uprava_view

--- client/src/pages/AdvUpdateForm/AdvUpdateForm.jsx
+++ client/src/pages/AdvUpdateForm/AdvUpdateForm.jsx
@@ -1,6 +1,8 @@
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { updateAdv, getAdv } from "../../models/Adv";
+import Home from "../img/home.png"
+import Bazar from "../img/bazar.png"
 
 export default function AdvUpdateForm() {
   const { id } = useParams();
@@ -59,22 +61,98 @@ export default function AdvUpdateForm() {
 
   return (
     <>
-      <h1>Adv update form</h1>
-      <p>{id}</p>
-      <form>
-      <input type="text" name="name" required placeholder="Nadpis inzerátu" onChange={e => handleChange(e)}/>
-        <input type="number" name="phone" required placeholder="Zadejte telefonní číslo" onChange={e => handleChange(e)}/>
-        <input type="text" name="email" required placeholder="Zadejte email" onChange={e => handleChange(e)}/>
-        <input type="number" name="price" required placeholder="Zadejte cenu" onChange={e => handleChange(e)}/>
-        <input type="text" name="description" required placeholder="Popis inzerátu" onChange={e => handleChange(e)}/>
-        <input type="text" name="ownername" required placeholder="Vaše jméno" onChange={e => handleChange(e)}/>
-        <input type="text" name="locality" required placeholder="Popis" onChange={e => handleChange(e)}/>
-        <input accept="image/*" type="file" name="img" required placeholder="Popis inzerátu" onChange={e => handleChange(e)}/>
-        <button onClick={handlePost}>Upravit</button>
-      </form>
-      <Link to={"/"}>
-        <p>Go back</p>
+          <div id="container">
+        <div className="title_header">
+          <div className="left__">
+          <Link to={"/"}>
+      <img className="img_home" src={Home} alt="" />
       </Link>
+          </div>
+            <div className="middle__">
+            <div className="middle_title">
+              BAZOŠ
+            </div>
+            </div>
+            <div className="right__">
+            <img className="img_home" src={Bazar} alt="" />
+            </div>
+        </div>
+        <div className="section_title">
+<div className="section_title_inside">
+  Úprava inzerátu
+</div>
+    </div>
+    <div className="create_container">
+      <form>
+        <div className="description_create_">
+          <div className="title_of_description">
+            Titulek
+          </div>
+      <input className="input_create" type="text" name="name" required placeholder="Nadpis inzerátu" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Telefon
+          </div>
+          <input onKeyPress={(event) => {
+        if (!/[0-9]/.test(event.key)) {
+          event.preventDefault();
+        }
+      }} className="input_create" type="number" name="phone" required placeholder="Zadejte telefonní číslo" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Email
+          </div>
+          <input className="input_create" type="text" name="email" required placeholder="Zadejte email" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Cena
+          </div>
+          <input onKeyPress={(event) => {
+        if (!/[0-9]/.test(event.key)) {
+          event.preventDefault();
+        }
+      }} className="input_create" type="number" name="price" required placeholder="Zadejte cenu" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Popisek
+          </div>
+          
+          <input className="input_create_description" type="text" name="description" required placeholder="Popište produkt" onChange={e => handleChange(e)}/>
+          
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Jméno
+          </div>
+          <input className="input_create" type="text" name="ownername" required placeholder="Zadejte jméno" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Adresa
+          </div>
+          <input className="input_create" type="text" name="locality" required placeholder="Zadejte adresu" onChange={e => handleChange(e)}/>
+      </div>
+
+      <div className="description_create">
+          <div className="title_of_description">
+            Heslo
+          </div>
+          <input className="input_create" type="text" name="password" required placeholder="Zadejte heslo" onChange={e => handleChange(e)}/>
+      </div>
+      </form>
+      <button onClick={handlePost}>Upravit</button>
+      </div>
+    </div>
     </>
   );
 }
--- client/src/pages/AdvView/AdvView.jsx
+++ client/src/pages/AdvView/AdvView.jsx
@@ -3,6 +3,7 @@ import { getAdv, deleteAdv } from "../../models/Adv";
 import { useState, useEffect } from "react";
 import Home from "../img/home.png"
 import Bazar from "../img/bazar.png"
+import Car from "../img/car_pink.png"
 
 export default function AdvView() {
   const { id } = useParams();
@@ -83,10 +84,39 @@ export default function AdvView() {
             {adv.name}
           </div>
         </div>
+        <div className="view_img_">
+            <img className="view_img_inside" src={Car} alt=""/>
+        </div>
+        <div className="description_of_adv">
+          <div className="title_of_description_">
+            <div className="title_of_description_inside">
+              Popisek 
+            </div>
+          </div>
+          <div className="description_of_adv_inside">
+            {adv.description}
+          </div>
+        </div>
+        <div className="title_of_description_">
+            <div className="title_of_description_inside">
+              Informace 
+            </div>
+          </div>
+        <div className="informations_container">
+          <div className="info_view">Cena</div>
+          <div className="info_view_second">{adv.price} Kč</div>
+          <div className="info_view">Telefon</div>
+          <div className="info_view_second">{adv.phone}</div>
+          <div className="info_view">Adresa</div>
+          <div className="info_view_second">{adv.locality}</div>
+          <div className="info_view">Email</div>
+          <div className="info_view_second">{adv.email}</div>
+          <div className="info_view">Jméno</div>
+          <div className="info_view_second">{adv.ownername}</div>
+        </div>
       <form>
         <input type="text" placeholder="Heslo" onChange={handleChange} />
         <button onClick={handleDelete}>Delete</button>
-        <p>{info}</p>
       </form>
       <Link to={`/updateadv/${id}`}>
         <p>Úprava inzerátu</p>
